6e16b541941a5320a7c66250ba89b510c90764f9
simonkimi <805757448@qq.com>
2020-11-08 01:18:32 +0800

    根据header自动对response进行decode

--- lib/network/http.dart
+++ lib/network/http.dart
@@ -1,6 +1,7 @@
 import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 import 'package:wust_life/util/debug.dart';
+import 'dart:io';
 
 class Http {
   static final Http _http = Http._internal();
@@ -11,7 +12,7 @@ class Http {
 
   final fiddlerProxy = "PROXY 192.168.2.199:8888";
   final localBaseUrl = "http://192.168.2.199:8000";
-  final finalUrl = "";
+  final remoteUrl = "";
 
   Dio _dio;
   var isLogin = false;
@@ -33,15 +34,26 @@ class Http {
       }
       // localhost
       _dio.options.baseUrl =
-          await connectLocalhost("8000") ? localBaseUrl : finalUrl;
+          await connectLocalhost("8000") ? localBaseUrl : remoteUrl;
     }
   }
 
+  Future<String> get<T>() async {
+    var response = await _dio.get<List<int>>("",
+        options: Options(responseType: ResponseType.bytes));
+    var encode = (response.headers.map["encode"] ?? [""])[0];
+    var crypto = encode.split(",");
+    var bytes = response.data;
+    if (crypto.contains("zlib")) {
+      var zlib = ZLibCodec();
+      bytes = zlib.decode(response.data);
+    }
+    if (crypto.contains("gzip")) {
+      var gzip = GZipCodec();
+      bytes = gzip.decode(response.data);
+    }
 
 
-  Future<Response<T>> get<T>() async {
-    var response = await _dio.get<T>("test");
-    print(response.headers.map);
-    return response;
+    return String.fromCharCodes(bytes);
   }
 }
